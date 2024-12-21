bad27cadfba5d3737348667d8df671db0afa4e88
vincent <vincent-huber@live.fr>
2024-12-21 19:38:42 +0100

    feat:changement de nom de dossier pour Old + récupération des ads dans index

--- Etnair_vue/pages/index.vue
+++ Etnair_vue/pages/index.vue
@@ -1,3 +1,19 @@
+<script setup>
+
+const ads = ref(null)
+ads.value = getAds()
+
+
+async function getAds(){
+    return await $fetch("/api/ads")
+}
+
+
+console.log('ads : ', ads.value)
+
+
+</script>
+
 <template>
     <div>
         <h1>HomePage</h1>
