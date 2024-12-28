f1fa2808b82e41464473e413091cb21518ccb27d
vincent <vincent-huber@live.fr>
2024-12-28 13:52:21 +0100

    feat(less): installation de less, création des différentes tailles de typo et de boutons

--- Etnair_vue/pages/index.vue
+++ Etnair_vue/pages/index.vue
@@ -1,21 +1,56 @@
 <script setup>
+const ads = ref(null);
+ads.value = getAds();
 
-const ads = ref(null)
-ads.value = getAds()
-
-
-async function getAds(){
-    return await $fetch("/api/ads")
+async function getAds() {
+  return await $fetch("/api/ads");
 }
-
-
-console.log('ads : ', ads.value)
-
-
 </script>
 
 <template>
-    <div>
-        <h1>HomePage</h1>
-    </div>
+  <div class="content wrapper is-grid">
+    <h1 class="content__titre">Ventalon en Cévennes</h1>
+    <h2 class="content__titre">Mes informations</h2>
+    <h3 class="content__titre">
+      • 5 pièces • 119m2 • 3 personnes • Jardin • Tossiat (01250)
+    </h3>
+    <p>
+      À louer à Tossiat, charmante maison de 119 m² idéale pour 3 personnes,
+      composée de 5 pièces, dont un séjour lumineux, une cuisine équipée et 2 ou
+      3 chambres selon vos besoins. Cette maison offre un jardin privatif
+      parfait pour la détente, une vue imprenable sur les montagnes, ainsi qu’un
+      accès Wi-Fi inclus.
+    <br/>
+    <br/>
+      Située dans un environnement paisible et verdoyant,
+      elle allie la tranquillité du village à la proximité des commodités
+      locales, idéale pour les amoureux de la nature et les activités de plein
+      air. Disponible dès maintenant, contactez-nous pour une visite et
+      découvrez cette opportunité unique !
+    </p>
+    <NuxtLink class="cta-primary">Se connecter</NuxtLink>
+    <NuxtLink class="cta-primary-outline">Se connecter</NuxtLink>
+    <NuxtLink class="cta-secondary">Modifier vos informations</NuxtLink>
+  </div>
 </template>
+
+<style lang="less">
+@import "@/assets/less/_import.less";
+
+.content {
+  * {
+    grid-column: 3 / 11;
+  }
+
+  @media @mobile {
+    & > * {
+      grid-column: 1 / 7;
+    }
+  }
+
+  &__titre {
+  }
+  .h-cta-primary {
+  }
+}
+</style>
