cedfa44c0460358e55601e4ff4629f43df2cfa83
vincent <vincent-huber@live.fr>
2025-01-02 22:03:41 +0100

    feat(user): création du cadre avec les infos user

--- Etnair_vue/pages/user.vue
+++ Etnair_vue/pages/user.vue
@@ -1,24 +1,47 @@
 <script setup>
-import '../assets/less/pages/user.less'
- 
+import "../assets/less/pages/user.less";
+
 const authStore = useAuthStore();
 
 // Fonction pour vérifier l'accès à la page
 const accessPage = () => {
-  if (!authStore.isAuthenticated) { 
-    return navigateTo('/');
+  if (!authStore.isAuthenticated) {
+    return navigateTo("/");
   }
 };
 
 onMounted(() => {
   accessPage();
 });
-
-
 </script>
 
 <template>
-    <div>
-        <h1>User page</h1>
+  <div class="user wrapper is-grid">
+    <h2 class="user__pageTitle">Mes informations</h2>
+    <div class="user__infos">
+      <CldImage
+        :src="authStore.user.picture"
+        class="infos__pictureProfile"
+        alt="Photo de profil"
+      />
+      <div class="infos__container">
+        <div class="container__nickname">
+          <p class="nickname__title">Nom d'utilisateur</p>
+          <p class="nickname__text">{{ authStore.user.nickname }}</p>
+        </div>
+        <div class="container__email">
+          <p class="email__title">Email</p>
+          <p class="email__text">{{ authStore.user.email }}</p>
+        </div>
+        <div class="container__password">
+          <p class="password__title">Mot de passe</p>
+          <p class="password__text">********</p>
+        </div>
+        <div class="container__button">
+          <NuxtLink class="cta-secondary">Modifier mes informations</NuxtLink>
+          <NuxtLink class="cta-secondary">Déconnexion</NuxtLink>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
