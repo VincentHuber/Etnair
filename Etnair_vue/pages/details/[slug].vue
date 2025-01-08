d8b526fbbfc4794299e4788fa9c4bb2b81f8fc9a
vincent <vincent-huber@live.fr>
2025-01-08 23:52:26 +0100

    feat(slug): affichage du titre, du nom de proprio, de sa photo, de la photo du bien, de sa description, de ses infos et de ses features

--- Etnair_vue/pages/details/[slug].vue
+++ Etnair_vue/pages/details/[slug].vue
@@ -1,10 +1,67 @@
 <script setup>
-import '../assets/less/pages/details.less'
+import "../assets/less/pages/details.less";
 
+const data = ref(null);
+const route = useRoute();
+
+// Import des icons
+import { featuresInfos } from "@/utils/featuresInfos";
+
+// Récupère l'annonce
+const fetchData = async () => {
+  try {
+    data.value = await $fetch(`/api/ad/${route.params.slug}`, {
+      method: "get",
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Le slug est accessible uniquement quand le DOM est monté
+onMounted(() => {
+  fetchData();
+});
 </script>
 
 <template>
-    <div class="wrapper is-grid">
-        <h1>DETAILS</h1>
+  <div v-if="data && data.ad" class="ad wrapper is-grid">
+    <div class="ad__adRenter">
+      <CldImage
+        class="adRenter__picture"
+        :src="data.ad.renter.picture"
+        alt="Photos du propriétaire"
+        height="100"
+        width="100"
+      />
+      <NuxtLink class="cta-secondary adRenter__nickname">
+        {{ data.ad.renter.nickname }}
+      </NuxtLink>
     </div>
+    <h1 class="ad__adTitle">{{ data.ad.title }}</h1>
+    <h3 class="ad__adInfos">
+      {{ data.ad.size }} m<sup>2</sup> • {{ data.ad.number_of_rooms }} pièces •
+      {{ data.ad.number_of_guests }} personnes • {{ data.ad.city }} ({{
+        data.ad.zipcode
+      }})
+    </h3>
+
+    <CldImage
+      class="ad__adPicture"
+      :src="data.ad.pictures[0]"
+      alt="Photos de couverture du bien"
+      height="1000"
+    />
+    <p>{{ data.ad.description }}</p>
+
+    <div class="ad__adFeatures">
+
+        <div
+          v-for="(feature, key) in data.ad.features"
+          :key="key"
+        >
+          <Features :text="feature" />
+        </div>
+    </div>
+  </div>
 </template>
