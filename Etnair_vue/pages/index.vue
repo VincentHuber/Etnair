53a845784efd09b311f78f0187e20e770c8e8c41
vincent <vincent-huber@live.fr>
2025-01-08 19:14:54 +0100

    feat(index): mise en place du slider

--- Etnair_vue/pages/index.vue
+++ Etnair_vue/pages/index.vue
@@ -1,7 +1,19 @@
 <script setup>
 import "../assets/less/pages/index.less";
+import { useKeenSlider } from "keen-slider/vue.es";
+import "keen-slider/keen-slider.min.css";
 
-const {data} = useFetch("/api/ads");
+// Déclare la référence du conteneur du slider
+const [container] = useKeenSlider({
+  loop: true,
+  mode: "free-snap",
+  slides: {
+    perView: 1.5,
+    spacing: 35,
+    origin: "center",
+  },
+});
+const { data } = useFetch("/api/ads");
 
 onMounted(() => {
   console.log("data Ads : ", data.value);
@@ -9,29 +21,28 @@ onMounted(() => {
 </script>
 
 <template>
-  <div class="content wrapper is-grid">
-    <h1> HELLO</h1>
-    
-    <div v-if="data.allAds">
-    <div v-for="(ad, key) in data.allAds" :key="key">
-
-     
-      <p>{{ ad.price.toString() }}€ par nuit</p>
-      <p>
-        {{ ad.size.toString() }}m2 •
-        {{ ad.number_of_rooms.toString() }} pièces •
-        {{ ad.number_of_guests.toString() }} personnes•
-        {{ ad.city}} ({{ad.zipcode.toString()}})
-      </p>
-      <CldImage
-          :src="ad.pictures[0]"
-          alt="Photos de la propriété"
-    
-        />
-
+  <div class="home">
+    <div v-if="data.allAds" class="home__result">
+      <div ref="container" class="keen-slider result__container ">
+        <div
+          v-for="(ad, key) in data.allAds"
+          :key="key"
+          class="keen-slider__slide container__loop "
+        >
+          <Ad
+            :renter="ad.renter.picture"
+            :title="ad.title"
+            :price="ad.price"
+            :size="ad.size"
+            :number_of_guests="ad.number_of_guests"
+            :number_of_rooms="ad.number_of_rooms"
+            :city="ad.city"
+            :zipcode="ad.zipcode"
+            :pictures="ad.pictures"
+            class="loop__ad"
+          />
+        </div>
+      </div>
     </div>
-    </div>
-
-    <Nuxtlink class="cta-primary">En savoir plus</Nuxtlink>
   </div>
 </template>
