0d544372c0c4199b5d5b075900a37cc32183ae17
vincent <vincent-huber@live.fr>
2025-01-08 22:01:11 +0100

    feat(details): renommage des fichiers

--- Etnair_vue/pages/details/[slug].vue
+++ Etnair_vue/pages/details/[slug].vue
@@ -1,10 +1,10 @@
 <script setup>
-import '../assets/less/pages/to-learn-more.less'
+import '../assets/less/pages/details.less'
 
 </script>
 
 <template>
     <div class="wrapper is-grid">
-        <h1>To learn more</h1>
+        <h1>DETAILS</h1>
     </div>
 </template>
