92b4ae711d2c4c180feb426c6d675e7ad80ac820
vincent <vincent-huber@live.fr>
2025-01-08 20:21:54 +0100

    feat(Ad): opacité sur les slides non active + génération du prix et features

--- Etnair_vue/pages/index.vue
+++ Etnair_vue/pages/index.vue
@@ -3,6 +3,14 @@ import "../assets/less/pages/index.less";
 import { useKeenSlider } from "keen-slider/vue.es";
 import "keen-slider/keen-slider.min.css";
 
+//Détermine l'index de la slide
+const sliderCurrent = ref(0);
+
+//Fonction pour les datas de mon slider
+const updateArrow = (s) => {
+  sliderCurrent.value = s.track.details.rel;
+};
+
 // Déclare la référence du conteneur du slider
 const [container] = useKeenSlider({
   loop: true,
@@ -12,6 +20,7 @@ const [container] = useKeenSlider({
     spacing: 35,
     origin: "center",
   },
+  detailsChanged: updateArrow,
 });
 const { data } = useFetch("/api/ads");
 
@@ -22,26 +31,35 @@ onMounted(() => {
 
 <template>
   <div class="home">
+    <div class="home__infos">
+      <p class="infos__price">
+        {{ data.allAds[sliderCurrent].price }}€<span>/nuit</span>
+      </p>
+
+      <p class="infos__details">
+        {{ data.allAds[sliderCurrent].size }} m<sup>2</sup> •
+        {{ data.allAds[sliderCurrent].number_of_rooms }} pièces •
+        {{ data.allAds[sliderCurrent].number_of_guests }} personnes •
+        {{ data.allAds[sliderCurrent].city }} ({{
+          data.allAds[sliderCurrent].zipcode
+        }})
+      </p>
+      <NuxtLink class="cta-primary">Découvrir</NuxtLink>
+    </div>
+
     <div v-if="data.allAds" class="home__result">
-      <div ref="container" class="keen-slider result__container ">
-        <div
+      <div ref="container" class="keen-slider result__container">
+        <NuxtLink
           v-for="(ad, key) in data.allAds"
           :key="key"
-          class="keen-slider__slide container__loop "
+          class="keen-slider__slide container__loop"
+          :style="{
+            opacity: key === sliderCurrent ? 1 : 0.3,
+            transition: 'opacity 0.5s ease',
+          }"
         >
-          <Ad
-            :renter="ad.renter.picture"
-            :title="ad.title"
-            :price="ad.price"
-            :size="ad.size"
-            :number_of_guests="ad.number_of_guests"
-            :number_of_rooms="ad.number_of_rooms"
-            :city="ad.city"
-            :zipcode="ad.zipcode"
-            :pictures="ad.pictures"
-            class="loop__ad"
-          />
-        </div>
+          <Ad :pictures="ad.pictures" class="loop__ad" />
+        </NuxtLink>
       </div>
     </div>
   </div>
