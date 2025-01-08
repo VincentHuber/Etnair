7f68abeed5927233192fc8b2a7a1a7771949111f
vincent <vincent-huber@live.fr>
2025-01-08 20:21:13 +0100

    feat(Ad): suppression des datas inutiles

--- Etnair_vue/components/Ad/Ad.vue
+++ Etnair_vue/components/Ad/Ad.vue
@@ -2,49 +2,10 @@
 import "./Ad.less";
 
 const props = defineProps({
-  price: {
-    type: Number,
-    required: true,
-  },
-
-  size: {
-    type: Number,
-    required: true,
-  },
-
-  number_of_rooms: {
-    type: Number,
-    required: true,
-  },
-
-  number_of_guests: {
-    type: Number,
-    required: true,
-  },
-
   pictures: {
     type: Array,
     required: true,
   },
-
-  city: {
-    type: String,
-    required: true,
-  },
-
-  title: {
-    type: String,
-    required: true,
-  },
-
-  zipcode: {
-    type: Number,
-    required: true,
-  },
-  renter:{
-    type:String,
-    required:true,
-  }
 });
 </script>
 
@@ -57,30 +18,6 @@ const props = defineProps({
     alt="Photos de la propriété"
     height="1000"
     />
-    <CldImage
-    class="ad__renter"
-    :src="renter"
-    alt="Photos de la propriété"
-    height="50"
-    width="50"
-    />
-    <div class="ad__details">
-
-    <p class="details__price">
-      {{ price.toString() }}€<span class="price__night">/nuit</span>
-    </p>
-    
-    <!-- <Nuxtlink class="container__button cta-primary">En savoir plus</Nuxtlink> -->
-</div>
-    <div class="ad__container">
-      <h1 class="container__title">{{ title }}</h1>
-      <p class="container__infos">
-          {{ size.toString() }}m<sup>2</sup> • {{ number_of_rooms.toString() }} pièces •
-          {{ number_of_guests.toString() }} personnes • {{ city }} ({{
-              zipcode.toString()
-            }})
-      </p>
-    </div>
   
   </div>
 </template>
